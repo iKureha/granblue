e37b6925d85f8f8a64018d981507f41beb71f542
Ubuntu <ubuntu@ip-172-31-18-211.ap-northeast-1.compute.internal>
2018-10-03 07:32:15 +0000

    test page

--- GranBlue/GranBlueModel.bak/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Boss(models.Model):
-    name = models.CharField(max_length=15)
-    level = models.IntegerField()
-    code = models.CharField(max_length=8)
-
-
