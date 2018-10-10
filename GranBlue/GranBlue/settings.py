ee7f8722172106f5c9a506e108d2587b2c461353
Ubuntu <ubuntu@ip-172-31-18-211.ap-northeast-1.compute.internal>
2018-10-10 08:23:14 +0000

    change this bakup to main

--- GranBlue/GranBlue/settings.py
+++ GranBlue/GranBlue/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zbp@3kbu1oeln3=73oxd*h%%v&e3t0t!9-lgyw55me^3#)v))e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
